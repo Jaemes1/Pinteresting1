eb3857a9d2e89e7d9f847c25440bfc114f8fb009
Jaemes <jaemes.mcarthur@gmail.com>
2014-07-08 19:34:38 -0300

    Added and Setp Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ InvGators::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+#Not to set this to my host
+config.action_mailer.default_url_options = { host: 'invgators.herokuapp.com' }
+
 end
